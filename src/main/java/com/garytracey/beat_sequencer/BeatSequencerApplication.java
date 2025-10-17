89e545fe1c1954da8a968cceb70ce9eda87356c8
Gary Tracey <gtracey@gmail.com>
2025-10-17 16:44:57 +0100

    Initial commit: Setup Spring Boot foundation with database connection

---
+++ src/main/java/com/garytracey/beat_sequencer/BeatSequencerApplication.java
@@ -0,0 +1,13 @@
+package com.garytracey.beat_sequencer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatSequencerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatSequencerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/garytracey/beat_sequencer/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.garytracey.beat_sequencer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/api/health")
+    public String healthCheck() {
+        return "API is running!";
+    }
+}
