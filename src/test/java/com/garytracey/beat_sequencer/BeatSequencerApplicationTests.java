0d17b16ffb09082c8a4fa30cc5db8dfde94e4e8e
Gary Tracey <gtracey@gmail.com>
2025-10-17 16:44:57 +0100

    Initial commit: Setup Spring Boot foundation with database connection

---
+++ src/test/java/com/garytracey/beat_sequencer/BeatSequencerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garytracey.beat_sequencer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatSequencerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
